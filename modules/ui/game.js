978772d6c791d2b841f6920c4af6f6c7dd59d973
james-e-crandall <james.e.crandall@gmail.com>
2025-05-14 21:32:22 -0500

    visual remap, city names

---
+++ modules/ui/game.js
@@ -0,0 +1,25 @@
+window.UnitPortraitVisualRemaps = {
+    ...(window.UnitPortraitVisualRemaps || {}),
+    'UNIT_BYEOLMUBAN': { //your unit type
+        unitType: 'UNIT_COURSER',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_BYEOLMUBAN_2': { //your unit type
+        unitType: 'UNIT_KNIGHT',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_BYEOLMUBAN_3': { //your unit type
+        unitType: 'UNIT_LANCER',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_EUNGSA': { //your unit type
+        unitType: 'UNIT_SCOUT',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+}
+
+
+
+
+
+
